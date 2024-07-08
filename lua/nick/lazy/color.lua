5301656aa7dbc620a0ad0aebb42dbbbd31aad206
kuripa <n.fuhrmann@protonmail.com>
2024-07-08 18:57:50 +0200

    feat: more colors

--- lua/nick/lazy/color.lua
+++ lua/nick/lazy/color.lua
@@ -1,7 +1,36 @@
+function ColorMyPencils(color)
+    color = color or "catppuccin-mocha"
+    vim.cmd.colorscheme(color)
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
 return {
-    "RRethy/nvim-base16",
-    lazy = false,
-    config = function()
-        vim.cmd("colorscheme base16-catppuccin")
-    end,
+    {
+        "RRethy/nvim-base16",
+        lazy = false,
+    },
+
+    {
+        "rose-pine/neovim",
+        name = "rose-pine"
+    },
+
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
+
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            ColorMyPencils()
+        end
+    },
+
 }
