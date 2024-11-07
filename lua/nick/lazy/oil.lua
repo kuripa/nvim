b66c7e23cb9b59e2f246400918c80c2fef1c1edb
kuripa <n.fuhrmann@protonmail.com>
2024-11-07 22:18:33 +0100

    fix:  rename variable name

--- lua/nick/lazy/oil.lua
+++ lua/nick/lazy/oil.lua
@@ -9,8 +9,8 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
     config = function()
         require("oil").setup({})
-        local neogit = require('oil')
-        vim.keymap.set("n", "<leader>o", neogit.open, {})
+        local oil = require('oil')
+        vim.keymap.set("n", "<leader>o", oil.open, {})
     end,
     }
 }
